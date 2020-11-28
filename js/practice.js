a5cc59ba7afc8ff8d4229178f4e37cd7936390d8
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2020-11-28 18:20:02 +0200

    add basic files

---
+++ js/practice.js
@@ -0,0 +1,12 @@
+// let a = (1,5 - 1) * 2;
+// alert(a); // 8 (потому что запятая)
+
+// alert(+"Infinity"); //Infinity  (+ - конкатенация)
+// alert(-"Infinity"); //-Infinity
+
+// console.log(true + false) //1
+// console.log(true + 2) //3
+
+// console.log(0.1+0.2)
+// console.log((0.01 * 100 + 0.02 * 100) / 100);
+
