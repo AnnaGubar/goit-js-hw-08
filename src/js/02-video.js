ebe9df81c5784efd88026887d14e9b4d12246554
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-02-07 16:47:12 +0200

    adds second part of homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem(TIME_KEY)) ?? true;
+
+player.setMuted(true);
+
+player.on('timeupdate', throttle(timeLocalStorageHandler, 1000));
+
+// function timeLocalStorageHandler(data) {
+//   localStorage.setItem(TIME_KEY, data.seconds);
+//   localStorage.getItem(TIME_KEY);
+// }
+
+function timeLocalStorageHandler(data) {
+  try {
+    localStorage.setItem(TIME_KEY, data.seconds);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+
+  try {
+    localStorage.getItem(TIME_KEY);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
